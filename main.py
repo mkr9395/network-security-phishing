74389df2cead17610a3b3e54e35826f7222bdae0
Mohit Kumar <mkr9395@gmail.com>
2024-11-05 01:57:18 +0530

    data validation steps completed

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+
 import sys
 
 if __name__=='__main__':
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion = DataIngestion(dataingestionconfig)
+        data_ingestion = DataIngestion(dataingestionconfig)       
         
-        logging.info("Initiated Data Ingestion")
-        
+        logging.info("Initiated Data Ingestion")       
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
+        logging.info("Completed Data Ingestion")
+        print(dataingestionartifact) 
+        
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        
+        logging.info("Initiated Data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Completed Data Validation")
+        print(data_validation_artifact) 
+        
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -5,7 +5,7 @@ from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.utils.main_utils.utils import read_yaml_file
+from networksecurity.utils.main_utils.utils import read_yaml_file, write_yaml_file
 
 from scipy.stats import ks_2samp
 import os, sys
@@ -25,25 +25,25 @@ class DataValidation:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-        @staticmethod
-        def read_data(file_path)-> pd.DataFrame:
-            try:
-                return pd.read_csv(file_path)
-            except Exception as e:
-                raise NetworkSecurityException(e, sys)
+    @staticmethod
+    def read_data(file_path)-> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
             
-        def validate_number_of_columns(self, dataframe: pd.DataFrame)-> bool:
-            try:
-                number_of_columns = len(self._schema_config)
-                logging.info(f'required number of columns : {number_of_columns}')
-                logging.info(f'dataframe has columns : {len(dataframe.columns)}')
+    def validate_number_of_columns(self, dataframe: pd.DataFrame)-> bool:
+        try:
+            number_of_columns = len(self._schema_config)
+            logging.info(f'required number of columns : {number_of_columns}')
+            logging.info(f'dataframe has columns : {len(dataframe.columns)}')
                 
-                if len(dataframe.columns) == number_of_columns:
-                    return True
-                else:
-                    return False
-            except Exception as e:
-                raise NetworkSecurityException(e, sys)
+            if len(dataframe.columns) == number_of_columns:
+                return True
+            else:
+                return False
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
         
     def detect_dataset_drift(self, base_df, current_df, threshold=0.05)-> bool:
         try:
@@ -61,15 +61,17 @@ class DataValidation:
                     is_found = True
                     status = False
                 report.update({column: {
-                    "p_value": float(is_same_dist.p_value),
+                    "p_value": float(is_same_dist.pvalue),
                     "drift_status": is_found
                 }})
             
             drift_report_file_path = self.data_validation_config.data_drift_report_file_path
             
-            # create directory
+            # create directory and save the report as yaml file
             dir_path = os.path.dirname(drift_report_file_path)
             os.makedirs(dir_path,exist_ok=True)
+            write_yaml_file(file_path = drift_report_file_path, content=report)
+            
         except Exception as e:
             raise NetworkSecurityException(e, sys) 
      
@@ -96,7 +98,25 @@ class DataValidation:
             
             ## check the data drift
             
-                
+            status = self.detect_dataset_drift(base_df = train_dataframe, current_df=test_dataframe)
+            dir_path = os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            
+            train_dataframe.to_csv(
+                self.data_validation_config.valid_train_file_path, index = False, header = True)
+            
+            test_dataframe.to_csv(
+                self.data_validation_config.valid_test_file_path, index = False, header = True)
+            
+            
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path = self.data_validation_config.valid_train_file_path,
+                valid_test_file_path = self.data_validation_config.valid_test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path= None,
+                drift_report_file_path = self.data_validation_config.data_drift_report_file_path
+            )
                 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
