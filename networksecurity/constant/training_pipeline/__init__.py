9ff3fa15dfa7a9aa433743d999d6ba472e5f2954
Mohit Kumar <mkr9395@gmail.com>
2024-11-04 19:27:46 +0530

    inside data_schema schema.yaml file created

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -15,6 +15,9 @@ TRAIN_FILE_NAME: str = 'train.csv'
 TEST_FILE_NAME: str = 'test.csv'
 
 
+SCHEMA_FILE_PATH :str = os.path.join('data_schema', 'schema.yaml') # inside data_schema folder, we have schema.yaml file
+
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
