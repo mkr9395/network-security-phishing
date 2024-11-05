eefc3cb46bcd30ad8a8c3b22a592c1a99401b260
Mohit Kumar <mkr9395@gmail.com>
2024-11-06 00:34:49 +0530

    entity.config_entity updated with DataTransformationConfig

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -39,7 +39,7 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
-
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 """
 Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
@@ -48,6 +48,7 @@ Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
 
 # KNN Imputer class to replace nan values
 DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -126,5 +126,22 @@ class DataTransformationConfig:
         ----------
         training_pipeline_config (TrainingPipelineConfig): Configuration object for training pipeline.
         """
-        # Set data transformation directory path
-        self.data_transformation_dir: str =     
+        # Set the root directory path for data transformation
+        self.data_transformation_dir: str = os.path.join(training_pipeline_config.artifact_dir, 
+                                                         training_pipeline.DATA_TRANSFORMATION_DIR_NAME)
+        
+        # Set the file path for transformed training data
+        # Replaces the file extension from "csv" to "npy" to indicate numpy file format
+        self.data_transformation_train_file_path: str =  os.path.join(self.data_transformation_dir, 
+                                                                      training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+                                                                      training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+        
+        # Set the file path for transformed testing data
+        # Replaces the file extension from "csv" to "npy" to indicate numpy file format
+        self.data_transformation_dir_test_file_path: str = os.path.join(self.data_transformation_dir, 
+                                                                        training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR_NAME)
+        
+        # Set the file path for the saved transformation object (e.g., scaler, encoder)                                                                training_pipeline.TEST_FILE_NAME.replace("csv", "npy"),)
+        self.transformed_object_file_path: str = os.path.join(self.data_transformation_dir,
+                                                              training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+                                                              training_pipeline.PREPROCESSING_OBJECT_FILE_NAME)    
