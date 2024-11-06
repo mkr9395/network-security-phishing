e4bbc2988125a75de9df752effa729ef23f1ee4c
Mohit Kumar <mkr9395@gmail.com>
2024-11-07 04:41:27 +0530

    model trainer added with best model going to artifact folder

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -39,6 +42,14 @@ if __name__=='__main__':
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info("Completed Data Transformation\n")
         print(data_transformation_artifact) 
+        
+        
+        logging.info("Model training started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config = model_trainer_config,
+                                     data_transformation_artifact = data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model training artifact created \n") 
         
         
     except Exception as e:
