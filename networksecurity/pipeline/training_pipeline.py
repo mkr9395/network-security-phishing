5f54b35249308b6ce4848565fcb35b3f6c806aa1
Mohit Kumar <mkr9395@gmail.com>
2024-11-11 16:03:47 +0530

    s3_syncer file created inside cloud folder

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -26,6 +26,7 @@ from networksecurity.entity.artifact_entity import (
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = s3Sync()
         
              
     def start_data_ingestion(self):
@@ -102,6 +103,25 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
+    ## local artifact is going to s3 bucket    
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir, aws_bucket_url = aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    
+    ## local final model is going to s3 bucket
+    def sync_saved_model_to_s3(self):
+        
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model"
+            self.s3_sync.sync_folder_to_s3(folder = slef.training_pipeline_config.model_dir, aws_bucket_url = aws_bucket_url)
+        
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
         
     def run_pipeline(self):
         
@@ -114,6 +134,11 @@ class TrainingPipeline:
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact = data_validation_artifact)
             
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact= data_transformation_artifact)
+            
+            
+            self.sync_artifact_dir_to_s3()
+            
+            self.sync_saved_model_to_s3()
             
             return model_trainer_artifact
         
